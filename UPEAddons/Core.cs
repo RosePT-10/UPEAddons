edce02eb9e4ce1cdff9b74757ca6f0998989ee26
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-04 11:31:57 -0600

    Begun Project

---
+++ UPEAddons/Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(UPEAddons.Core), "UPEAddons", "1.0.0", "taldo", null)]
+[assembly: MelonGame("Videocult", "Airframe")]
+
+namespace UPEAddons
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
