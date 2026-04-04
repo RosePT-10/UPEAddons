d539abb2b29b1137e31f54b0f6587f374ffc27b9
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-04 14:57:30 -0600

    Progress 4: Loaded an Asset Bundle

--- UPEAddons/Core.cs
+++ UPEAddons/Core.cs
@@ -4,6 +4,7 @@ using System;
 using UnityEngine;
 using Il2CppView_Audio;
 using Il2CppMS.Internal.Xml.XPath;
+using Il2CppSystem.Net.Cache;
 
 [assembly: MelonInfo(typeof(UPEAddons.Core), "UPEAddons", "1.0.0", "RosePT-10", null)]
 [assembly: MelonGame("Videocult", "Airframe")]
@@ -16,39 +17,62 @@ namespace UPEAddons
         AudioClip noise_clip;
         public UnityEngine.GameObject jumpscare_game_object;
         UnityEngine.AudioSource noise_audio_source;
+        AssetBundle asset;
 
         bool y_or_n; // rng check
         string path;
         string path_to_noise_clip;
 
+        IEnumerator<AssetBundleCreateRequest> LoadFromMemoryAsync(string path)
+        {
+            AssetBundleCreateRequest createRequest = AssetBundle.LoadFromMemoryAsync
+            (
+                File.ReadAllBytes
+                (
+                    path
+                )
+            );
+            yield return createRequest;
+            AssetBundle bundle = createRequest.assetBundle;
+
+        }
         public override void OnInitializeMelon()
         {
+            // initialize asset bundle
+            LoadFromMemoryAsync("UPEAddons/jump_scare.assets");
+
+            
+            
             // find where the .wav should be stored
-            path = Application.dataPath;
-            int dotdot = path.LastIndexOf('/');
-            path = path.Remove(dotdot, 19); // go back one folder
-            path = path.Insert(dotdot, "/UserData/UPEAddons/AudioClip"); 
-            LoggerInstance.Msg(path);
-            path_to_noise_clip = path + "/Poke.wav";
-            LoggerInstance.Msg(path_to_noise_clip);
+            //path = Application.dataPath;
+            //int dotdot = path.LastIndexOf('/');
+            //path = path.Remove(dotdot, 19); // go back one folder
+            //path = path.Insert(dotdot, "/UserData/UPEAddons/AudioClip"); 
+            //path_to_noise_clip = path + "/Poke.wav";
+            //LoggerInstance.Msg(path);
+            //LoggerInstance.Msg(path_to_noise_clip);
 
             // get the .wav as an audioclip
-            
+            //noise_clip = Resources.Load<AudioClip>("Poke");
+            //noise_clip.name = "noise_clip";
+            //LoggerInstance.Msg(noise_clip.name);
             
             // initialize the jump scare audioclip
-            jumpscare_game_object = new UnityEngine.GameObject();
-            jumpscare_game_object.name = "jumpscare_game_object";
+            //jumpscare_game_object = new UnityEngine.GameObject();
+            //jumpscare_game_object.name = "jumpscare_game_object";
 
-            jumpscare_game_object.AddComponent<AudioSource>();
-            noise_audio_source = jumpscare_game_object.GetComponent<AudioSource>();
-            noise_audio_source.clip = noise_clip;
+            //jumpscare_game_object.AddComponent<AudioSource>();
+            //noise_audio_source = jumpscare_game_object.GetComponent<AudioSource>();
+            //noise_audio_source.clip = noise_clip;
+            
+            
             
             //noise_sound_player = UnityEngine.GameObject.Find("Audio_Main");
             //LoggerInstance.Msg(sound_file);
             //jump_scare = 
             //jump_scare.LoadAudioData();
 
-            LoggerInstance.Msg("Initialized.");
+            LoggerInstance.Msg("Successfully Initialized! Yipee!");
         }
 
         public override void OnUpdate()
@@ -56,10 +80,10 @@ namespace UPEAddons
             base.OnUpdate();
             
             // determine if we play noise this frame
-            if (CheckRng() == true)
-            {
+            //if (CheckRng() == true)
+            //{
                 //audio.Play();
-            }
+            //}
             
         }
 
--- UPEAddons/obj/Debug/net6.0/UPEAddons.AssemblyInfo.cs
+++ UPEAddons/obj/Debug/net6.0/UPEAddons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+110429779670c4a3ca38817dc8989ef21237798b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+33552945f4b1273af710f8976e19b73c293550e8")]
 [assembly: System.Reflection.AssemblyProductAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
