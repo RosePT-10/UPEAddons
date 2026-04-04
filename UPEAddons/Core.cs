f593ade32945b07de15b40d753926e1f2a1529ae
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-04 12:14:33 -0600

    Outline of mod

--- UPEAddons/Core.cs
+++ UPEAddons/Core.cs
@@ -1,4 +1,7 @@
 ﻿using MelonLoader;
+using Unity.Collections.LowLevel.Unsafe;
+using System;
+using UnityEngine;
 
 [assembly: MelonInfo(typeof(UPEAddons.Core), "UPEAddons", "1.0.0", "taldo", null)]
 [assembly: MelonGame("Videocult", "Airframe")]
@@ -7,9 +10,43 @@ namespace UPEAddons
 {
     public class Core : MelonMod
     {
+        AudioSource audio;
+        AudioClip jump_scare;
+        UnityEngine.GameObject sound_file;
+        bool y_or_n; // rng check
+
         public override void OnInitializeMelon()
         {
+            // initialize the jump scare audioclip
+            sound_file = GameObject.Find("AirframeCrash_01.wav");
+            //jump_scare = 
+            jump_scare.LoadAudioData();
+
             LoggerInstance.Msg("Initialized.");
         }
+
+        public override void OnUpdate()
+        {
+            base.OnUpdate();
+            
+            // determine if we play jumpscare this frame
+            if (CheckRng() == true)
+            {
+                audio.Play();
+            }
+            
+        }
+
+        private bool CheckRng()
+        {
+            int rng = System.Random.Shared.Next(0, 1000);
+            if (rng == 1)
+            {
+                //
+            }
+
+            return y_or_n;
+        }
+        
     }
 }
