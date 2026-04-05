32f52d7c4dcffa72586c53693c06149ade15d894
Tal-01 <rose.petal.thorn@tutamail.com>
2026-04-04 18:47:59 -0600

    Progress 7: Displayed frame

--- UPEAddons/Core.cs
+++ UPEAddons/Core.cs
@@ -7,6 +7,8 @@ using Il2CppMS.Internal.Xml.XPath;
 using Il2CppSystem.Net.Cache;
 using UnityEngine.ResourceManagement.ResourceProviders;
 using Il2CppSystem.Linq;
+using UnityEngine.UI;
+using MelonLoader.ICSharpCode.SharpZipLib.Zip;
 
 [assembly: MelonInfo(typeof(UPEAddons.Core), "UPEAddons", "1.0.0", "RosePT-10", null)]
 [assembly: MelonGame("Videocult", "Airframe")]
@@ -15,33 +17,37 @@ namespace UPEAddons
 {
     public class Core : MelonMod
     {
-        AudioSource audio;
-        AudioClip noise_clip;
         public UnityEngine.GameObject jumpscare_game_object;
-        UnityEngine.AudioSource noise_audio_source;
+        public UnityEngine.Texture jump_scare_texture;
         AssetBundle bundle;
 
+        int timer; // check once every second
         bool y_or_n; // rng check
         string path;
-        string path_to_noise_clip;
 
-        IEnumerator<AssetBundleCreateRequest> LoadFromMemoryAsync(string path)
+        private void DrawImage()
+        {   
+            LoggerInstance.Msg(jump_scare_texture);
+            GUI.DrawTexture(new Rect(0, 0, 1000, 1000), jump_scare_texture);
+        }
+        private bool CheckRng()
         {
-            AssetBundleCreateRequest createRequest = AssetBundle.LoadFromMemoryAsync
-            (
-                File.ReadAllBytes
-                (
-                    path
-                )
-            );
-            yield return createRequest;
-            bundle = createRequest.assetBundle;
+            int rng = System.Random.Shared.Next(0, 10000);
+            //LoggerInstance.Msg(rng);
+            if (rng == 1)
+            {
+                y_or_n = true;
+            }
+            else
+            {
+                y_or_n = false;
+            }
 
+            return y_or_n;
         }
         public override void OnInitializeMelon()
         {
             // initialize asset bundle
-            //LoadFromMemoryAsync("D:/_AIRFRAME ULTRA/_Mods and tools/_RoseMods/UPEAddons/UPEAddons/jump_scare.assets");
             bundle = AssetBundle.LoadFromFile("D:/_AIRFRAME ULTRA/_Mods and tools/_RoseMods/UPEAddons/UPEAddons/jump_scare.assets");
             if (bundle == null)
             {
@@ -51,48 +57,12 @@ namespace UPEAddons
             {
                 LoggerInstance.Msg("Loaded custom asset bundle");
             }
+
+            // initialize texture stuff
             
 
-            // testing
-            
-
-            
-            
-            // find where the .wav should be stored
-            //path = Application.dataPath;
-            //int dotdot = path.LastIndexOf('/');
-            //path = path.Remove(dotdot, 19); // go back one folder
-            //path = path.Insert(dotdot, "/UserData/UPEAddons/AudioClip"); 
-            //path_to_noise_clip = path + "/Poke.wav";
-            //LoggerInstance.Msg(path);
-            //LoggerInstance.Msg(path_to_noise_clip);
-
-            // get the .wav as an audioclip
-            //noise_clip = bundle.LoadAsset<AudioClip>("Poke");
-            //LoggerInstance.Msg(noise_clip.name);
-            
-            // initialize the jump scare audioclip
-            jumpscare_game_object = bundle.LoadAsset<GameObject>("JumpScare");
-            LoggerInstance.Msg(jumpscare_game_object.name);
-            noise_audio_source = jumpscare_game_object.GetComponent<AudioSource>();
-            LoggerInstance.Msg(noise_audio_source.name);
-            //noise_audio_source.clip = bundle.LoadAsset<AudioClip>("Poke");
-            AudioClip test = jumpscare_game_object.GetComponent<AudioSource>().clip;
-            if (test == null)
-            {
-                LoggerInstance.Msg("didn't work :[");
-            }
-            else
-            {
-                LoggerInstance.Msg("worked!!!!!!");
-            }
-            
-            
-            
-            //noise_sound_player = UnityEngine.GameObject.Find("Audio_Main");
-            //LoggerInstance.Msg(sound_file);
-            //jump_scare = 
-            //jump_scare.LoadAudioData();
+            // set timer
+            timer = 0;
 
             LoggerInstance.Msg("Successfully Initialized! Yipee!");
         }
@@ -100,37 +70,61 @@ namespace UPEAddons
         public override void OnSceneWasLoaded(int buildIndex, string sceneName)
         {
             base.OnSceneWasLoaded(buildIndex, sceneName);
+
+            // testing jumpscare
             if (sceneName == "MainMenu")
             {
-                jumpscare_game_object = bundle.LoadAsset<GameObject>("JumpScare");
-                LoggerInstance.Msg(jumpscare_game_object.name);
+                jumpscare_game_object = bundle.LoadAsset<GameObject>("JumpScareAudio");
                 GameObject.Instantiate(jumpscare_game_object);
-                LoggerInstance.Msg("survived");
                 jumpscare_game_object.GetComponent<AudioSource>().Play();
+                jump_scare_texture = bundle.LoadAsset<Texture>("AFU Beta Disclaimer");
+                Texture.Instantiate(jump_scare_texture);
+                if (jump_scare_texture == null)
+                {
+                    LoggerInstance.Msg("didnt work"); 
+                }
+                else
+                {
+                    LoggerInstance.Msg("Worked!"); 
+                }
+                MelonEvents.OnGUI.Subscribe(DrawImage, 0);
             }
         }
-        public override void OnUpdate()
-        {
-            base.OnUpdate();
-            
-            // determine if we play noise this frame
-            //if (CheckRng() == true)
-            //{
-                //audio.Play();
-            //}
-            
-        }
+        
 
-        private bool CheckRng()
+        public override void OnFixedUpdate()
         {
-            int rng = System.Random.Shared.Next(0, 1000);
-            if (rng == 1)
+            base.OnFixedUpdate();
+            // only check once every second
+            if (timer >= 50)
             {
-                //
-            }
+                timer = 0;
 
-            return y_or_n;
+                // play jumpscare this frame depending on rng
+                //LoggerInstance.Msg("Checking rng...");
+                CheckRng();
+                if (y_or_n == true)
+                {
+                    jumpscare_game_object = bundle.LoadAsset<GameObject>("JumpScareAudio");
+                    GameObject.Instantiate(jumpscare_game_object);
+                    jumpscare_game_object.GetComponent<AudioSource>().Play();
+                    LoggerInstance.Msg("Played jump scare sound this frame.");
+                }
+                else
+                {
+                    //LoggerInstance.Msg("\"You missed that one, try another!\"");
+                }
+            }
+            else
+            {
+                timer ++;
+                //LoggerInstance.Msg(timer);
+            }
+            
+            
         }
+
+        
 
         public override void OnApplicationQuit()
         {
--- UPEAddons/obj/Debug/net6.0/UPEAddons.AssemblyInfo.cs
+++ UPEAddons/obj/Debug/net6.0/UPEAddons.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c2a14a12c3c7f807d2414fc15a599a3233b0a0e9")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bc04ce55129150c42bdc50acf3689ac454570a5f")]
 [assembly: System.Reflection.AssemblyProductAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyTitleAttribute("UPEAddons")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
